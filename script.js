4a7a2c1bbca3ddcceadd7c580fcbd38532d7dbae
Anubhav Chaudhary <126059931+13anubhav@users.noreply.github.com>
2023-09-07 22:33:58 +0530

    Update script.js --> Added Copied Button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function copyToClipboard(text) {
+function copyToClipboard(text, button) {
     const textArea = document.createElement('textarea');
     textArea.value = text;
     document.body.appendChild(textArea);
@@ -8,9 +8,17 @@ function copyToClipboard(text) {
 
     const copySuccess = document.getElementById('copySuccess');
     copySuccess.style.opacity = '1';
+    
+    // Get the position of the button
+    const buttonRect = button.getBoundingClientRect();
+    
+    // Position the "Copied!" message to the right of the button
+    copySuccess.style.top = buttonRect.top + 'px';
+    copySuccess.style.left = (buttonRect.right + 10) + 'px';
+
     setTimeout(() => {
         copySuccess.style.opacity = '0';
     }, 1500);
 }
 
-copyToClipboard(text);
+copyToClipboard(text, button);
